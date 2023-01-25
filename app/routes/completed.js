abbc97bbc65833d5cbaffbb08ca9ea44e99886d6
Cameron Montgomery <cmontgomery@squareup.com>
2023-01-25 09:14:16 -0700

    update completed route with model hook

--- app/routes/completed.js
+++ app/routes/completed.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class CompletedRoute extends Route {
+  @service('todo-data') todos;
 
-export default class CompletedRoute extends Route {}
+  model() {
+    let todos = this.todos;
+
+    return {
+      get completedTodos() {
+        return todos.completed;
+      },
+    };
+  }
+}
